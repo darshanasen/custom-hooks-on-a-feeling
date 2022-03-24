da4e493905748526a371e0370681c7312441954b
Darshana Sen <darsh.sen@gmail.com>
2022-03-24 08:47:22 -0400

    add reusable logic example

---
+++ src/reusableLogic.js
@@ -0,0 +1,19 @@
+// Example of repeating logic in regular ol' JS
+
+const add = (a, b) => {
+    const sum = a + b;
+    if (sum > 10) {
+        console.log('Whoa big number');
+    } else {
+        console.log('Super small');
+    }
+}
+
+const subtract = (a, b) => {
+    const difference = a - b;
+    if (difference > 10) {
+        console.log('Whoa big number');
+    } else {
+        console.log('Super small');
+    }
+}
